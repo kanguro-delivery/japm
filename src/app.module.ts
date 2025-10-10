d3d5111d31c7541e3294f005266543cee084c770
sluceno <sluceno.a@gmail.com>
2025-10-10 14:55:45 +0200

    cowboy: Increment rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -75,7 +75,7 @@ import { ApiKeyOrJwtGuard } from './auth/guards/api-key-or-jwt.guard';
           {
             name: 'default',
             ttl: parseInt(configService.get('THROTTLE_TTL') || '60') * 1000,
-            limit: parseInt(configService.get('THROTTLE_LIMIT') || '500'),
+            limit: parseInt(configService.get('THROTTLE_LIMIT') || '20000'),
           },
         ];
       },
